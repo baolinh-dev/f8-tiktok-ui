17884544801010e58831e908ccf7a5f74f55247a
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-03-17 05:21:44 +0700

    Xây dựng Logic phần Header #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,28 +1,24 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import Image from '~/components/Image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
-const cx = classNames.bind(styles); 
+const cx = classNames.bind(styles);
 
-
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/187314f3e898d848befe1ac5a21ccc4b~c5_100x100.jpeg?x-expires=1677898800&x-signature=Z4V0b90sEgEpSTL8vujnMyQ2juM%3D"
-                alt="Avatar"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>nguyenvana</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 // Layouts
 import { HeaderOnly } from '~/components/Layout';
 // Pages
-import {Home, Following, Profile, Upload, Search} from '~/pages/';
+import { Home, Following, Profile, Upload, Search } from '~/pages/';
 // Public routes
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
+    { path: '/@:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 ];
